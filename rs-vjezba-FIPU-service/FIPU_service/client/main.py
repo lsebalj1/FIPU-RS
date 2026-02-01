9e728b61dd9cbd25cf87caa9e246f634b9dfdea6
lsebalj1 <lusebalj@student.unipu.hr>
2026-02-01 13:07:56 +0100

    3.zadatak

--- rs-vjezba-FIPU-service/FIPU_service/client/main.py
+++ rs-vjezba-FIPU-service/FIPU_service/client/main.py
@@ -1,2 +1,31 @@
 import asyncio
-from aiohttp 
+import aiohttp 
+
+URL = "http://127.0.0.1:8000"
+
+async def get_student(session):
+    async with session.get(f"{URL}/studenti") as response:
+        data = await response.json()
+        print("GEt studenti status {response.status}")
+        return data
+
+async def get_kolegij(session):
+    async with session.get(f"{URL}/kolegiji") as response:
+        data = await response.json()
+        print("Get kolegij status {response.status}")
+        return data
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        student_tasks = [get_student(session) for _ in range(50)]
+        kolegij_tasks = [get_kolegij(session) for _ in range(30)]
+    
+        all_tasks = student_tasks + kolegij_tasks
+        
+        results = await asyncio.gather(*all_tasks)
+        
+        print(f"Ukupno poslano zahtjeva {len(results)}")
+    
+asyncio.run(main())
+        
+    
